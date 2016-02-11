752b2484ffd9ea93344072a7b285de0ca983655e
i4c8 <i4c8@ugrad.cs.ubc.ca>
2016-02-11 00:38:18 -0800

    Implemented saveEvent method. Layout fixes for adding event

--- app/src/main/java/me/leafbit/todue/AddEventActivity.java
+++ app/src/main/java/me/leafbit/todue/AddEventActivity.java
@@ -6,9 +6,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TimePicker;
 
+import java.sql.Date;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class AddEventActivity extends AppCompatActivity {
 
@@ -19,8 +24,44 @@ public class AddEventActivity extends AppCompatActivity {
 
         //load all categories and display them in the list
         ArrayList<Category> categories = Category.loadAllCategories(this);
+        System.out.println("LOADED: " + categories.size() + " categories from database");
         ListView categoryView = (ListView) findViewById(R.id.addEventCatList);
         //TODO: Populate list with available categories
+    }
+
+    // Called when confirm button is clicked to save the event
+    public void onConfirm(View view){
+        // Find all fields
+        EditText nameText = (EditText) findViewById(R.id.addEventNameText);
+        CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
+        TimePicker timePicker = (TimePicker) findViewById(R.id.timePicker);
+        ListView categoryView = (ListView) findViewById(R.id.addEventCatList);
+
+        // Build event object
+        String name = nameText.getText().toString(); // name
+        if(name.isEmpty()){
+            //TODO: Alert user that the event must be named
+            System.out.println("DEBUG: Event left unnamed");
+            finish(); // Temporary debugging
+        }
+        // Not sure if this works; needs testing and type research
+        Date dueDate = new Date(calendarView.getDate()); // dueDate
+        // Current time
+        java.util.Date cur = new java.util.Date();
+        Date currentDate = new Date(cur.getTime()); // currentDate
+        if(dueDate.before(currentDate)){
+            //TODO: Alert user that the event must be in the future
+            // (Maybe don't have to enforce this)
+            System.out.println("DEBUG: Selected date before current date...");
+            finish();
+        }
+        // TODO: Get selected category from listview
+        Category category = new Category("Test", "#FFFF00"); // Temporary category
+
+        Event e = new Event(name, dueDate, currentDate, category);
+        e.saveEvent(e, this);
+
+
     }
 
     public void setDate(View view){
--- app/src/main/java/me/leafbit/todue/Event.java
+++ app/src/main/java/me/leafbit/todue/Event.java
@@ -1,23 +1,40 @@
 package me.leafbit.todue;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 
 import java.util.Calendar;
-import java.util.Date;
+import java.sql.Date;
 
 /**
  * Created by Adam on 2016-02-06.
  */
 public class Event {
 
+    String name;
     Calendar dueDate; // When the event is due. Includes YEAR, MONTH, DAY_OF_MONTH, AM_PM, HOUR
     Calendar createdDate; // When the event was created. Same fields as above
     Category category; // User defined category for this event
 
+    // alternate types for dates
+    Date dateDue;
+    Date dateCreated;
 
-    public Event(Calendar dueDate, Calendar createdDate, Category category){
+
+
+    public Event(String name, Calendar dueDate, Calendar createdDate, Category category){
+        this.name = name;
         this.dueDate = dueDate;
         this.createdDate = createdDate;
+        this.category = category;
+    }
+
+    public Event(String name, Date dateDue, Date dateCreated, Category category){
+        this.name = name;
+        this.dateDue = dateDue;
+        this.dateCreated = dateCreated;
         this.category = category;
     }
 
@@ -30,8 +47,26 @@ public class Event {
     }
 
     // Save an event to storage
-    public void saveEvent(Event e){
-        //TODO:
+    public long saveEvent(Event e, Context ctx){
+        // Instantiate Database
+        ToDueDbHelper dbHelper = new ToDueDbHelper(ctx);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        // Select columns for value insertion
+        ContentValues values = new ContentValues();
+        values.put(ToDueContract.EventEntry.COLUMN_NAME_EVENT_NAME, e.name); //name
+        values.put(ToDueContract.EventEntry.COLUMN_NAME_DUE, e.dateCreated.getTime()); //duedate
+        values.put(ToDueContract.EventEntry.COLUMN_NAME_START, e.dateCreated.getTime()); //createddate
+        values.put(ToDueContract.CategoryEntry.COLUMN_NAME_NAME, e.category.id); // category name
+
+        long newRowId;
+        newRowId = db.insert(
+                ToDueContract.EventEntry.TABLE_NAME,
+                "null",
+                values
+        );
+
+        return newRowId;
+
     }
 
 
